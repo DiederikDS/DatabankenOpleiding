7aff49ec39ecac7db883e771115e3620f3ec84fb
DiederikDS <diederik.desoete96@gmail.com>
2020-09-22 19:08:51 +0200

    Update 009.sql

--- ModuleDatabanken/01-SELECT-met-en-zonder-FROM/009.sql
+++ ModuleDatabanken/01-SELECT-met-en-zonder-FROM/009.sql
@@ -1,4 +1,5 @@
 ﻿-- Geef de landen waarin de klanten wonen (kolom country in tabel customer).  
 -- Zorg dat elk land maar 1 keer in het resultaat voorkomt
 
-
+SELECT distinct Country
+		FROM Customer
