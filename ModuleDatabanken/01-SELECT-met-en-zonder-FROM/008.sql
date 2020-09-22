e0b954623ddc58bf73e6b0ef801356e9d04fb72e
DiederikDS <diederik.desoete96@gmail.com>
2020-09-22 19:07:28 +0200

    Update 008.sql

--- ModuleDatabanken/01-SELECT-met-en-zonder-FROM/008.sql
+++ ModuleDatabanken/01-SELECT-met-en-zonder-FROM/008.sql
@@ -1,4 +1,7 @@
 ﻿-- Geef voornaam, familienaam en land van alle klanten (tabel customer).  
 -- Zorg dat de kolomnaam in het Nederlands wordt weergegeven
 
-
+SELECT FirstName as Voornaam
+		, LastName as Achternaam
+		, Country as Land
+		FROM Customer
