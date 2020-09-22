7ba5dac57e5ee26dcffd9f7f08993e2d645cbfcb
DiederikDS <diederik.desoete96@gmail.com>
2020-09-22 20:19:50 +0200

    Update 003.sql

--- ModuleDatabanken/01-SELECT-met-en-zonder-FROM/003.sql
+++ ModuleDatabanken/01-SELECT-met-en-zonder-FROM/003.sql
@@ -9,3 +9,14 @@ SELECT SQRT(2)
 	, SQRT(SQRT(2) + SQRT(30))
 
 2,62515506921491
+
+--Ander voorbeeld
+
+SELECT vkw2
+	, vkw30
+	, vkw2 + vkw30
+	, sqrt(vkw2 + vkw30)
+FROM (
+	SELECT sqrt(2) vkw2
+		, sqrt(30) vkw30
+	) AS vkws
