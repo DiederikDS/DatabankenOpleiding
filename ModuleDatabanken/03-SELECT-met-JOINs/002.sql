94b49963ab686064e631b984bf5c1a6fe722678b
Philip van Oosten <pvoosten@gmail.com>
2020-10-01 21:30:20 +0200

    Oefeningenreeks 3

---
+++ ModuleDatabanken/03-SELECT-met-JOINs/002.sql
@@ -0,0 +1,3 @@
+﻿-- Maak een overzicht van albumnaam en artiestnaam voor alle albums die terug te vinden zijn in de Chinook database.
+
+
---
+++ ModuleDatabanken/03-SELECT-met-JOINs/003.sql
@@ -0,0 +1,4 @@
+﻿-- Maak voor alle tracks een overzicht van tracknaam, albumnaam, artiestnaam, naam mediatype, naam genre.
+-- Hou er rekening mee dat een track niet altijd een genre heeft. Een track heeft ook niet altijd een album.
+
+
