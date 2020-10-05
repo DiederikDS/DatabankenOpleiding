1c5117d6fff1dd01507d71e0daf376def3d1ed40
DiederikDS <diederik.desoete96@gmail.com>
2020-10-05 21:40:49 +0200

    Update 001.sql
    Oplossing Oefening 1

--- ModuleDatabanken/03-SELECT-met-JOINs/001.sql
+++ ModuleDatabanken/03-SELECT-met-JOINs/001.sql
@@ -1,4 +1,9 @@
 ﻿-- Hoeveel artiesten zijn er die geen album hebben? Of minder specifiek verwoord: zijn er artiesten opgeslagen in de database waarvoor er geen album is?
 -- Los op met een outer join en met IS (NOT) NULL in de WHERE clause. 
 
+SELECT COUNT(ISNULL(NULL, Name))
+FROM Album
+FULL OUTER JOIN Artist
+ON Album.AlbumId = Artist.ArtistId
+
 
