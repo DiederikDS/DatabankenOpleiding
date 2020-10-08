f8f0243c546f99bbf1e1e950679d4c6c258032d5
DiederikDS <diederik.desoete96@gmail.com>
2020-10-08 20:41:24 +0200

    Update 001.sql
    Oplossing oefening

--- ModuleDatabanken/05-SELECT-met-de-set-operatoren-UNION-EXCEPT-INTERSECT/001.sql
+++ ModuleDatabanken/05-SELECT-met-de-set-operatoren-UNION-EXCEPT-INTERSECT/001.sql
@@ -1,4 +1,6 @@
 ﻿-- Maak een lijst van alle voornamen die zowel de voornaam van een Employee als van een Customer zijn.
 -- Gebruik daarvoor 2 queries die je combineert met INTERSECT.
 
-
+SELECT DISTINCT *
+FROM 
+(SELECT FirstName FROM Employee UNION SELECT FirstName FROM Customer) as Naam
