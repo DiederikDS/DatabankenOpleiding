0cfb46c7d4cbb2a6d863265f9a37219873dde756
DiederikDS <diederik.desoete96@gmail.com>
2020-10-08 20:15:56 +0200

    Update 002.sql

--- ModuleDatabanken/02-SELECT-met-WHERE/002.sql
+++ ModuleDatabanken/02-SELECT-met-WHERE/002.sql
@@ -1,4 +1,8 @@
 ﻿-- Geef voornaam en familienaam van alle klanten die in België of Frankrijk wonen  
 -- Gebruik OR
 
+SELECT FirstName
+	, LastName
+FROM Customer
+WHERE Country = 'Belgium' OR Country = 'France'
 
