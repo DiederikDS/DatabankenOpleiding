47a30b1e561a9e31e1694a6450caea5eca0f872e
DiederikDS <diederik.desoete96@gmail.com>
2020-10-08 20:43:09 +0200

    Update 002.sql
    Oplossing oefeningen

--- ModuleDatabanken/05-SELECT-met-de-set-operatoren-UNION-EXCEPT-INTERSECT/002.sql
+++ ModuleDatabanken/05-SELECT-met-de-set-operatoren-UNION-EXCEPT-INTERSECT/002.sql
@@ -1,4 +1,7 @@
 ﻿-- Maak een lijst van alle voornamen van Customers die geen voornaam zijn van een Employee.
 -- Gebruik daarvoor 2 queries die je combineert met EXCEPT.
 
+SELECT DISTINCT *
+FROM 
+(SELECT FirstName FROM Customer EXCEPT SELECT FirstName FROM Employee) as Naam
 
