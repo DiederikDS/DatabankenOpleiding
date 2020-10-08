25fde3bc68a18825e53e2917e1cd7d63da6010a5
DiederikDS <diederik.desoete96@gmail.com>
2020-10-08 14:43:39 +0200

    Update 002.sql
    Oplossing oefening 2

--- ModuleDatabanken/03-SELECT-met-JOINs/002.sql
+++ ModuleDatabanken/03-SELECT-met-JOINs/002.sql
@@ -1,3 +1,9 @@
 ﻿-- Maak een overzicht van albumnaam en artiestnaam voor alle albums die terug te vinden zijn in de Chinook database.
 
+SELECT Name as Artiestnaam
+	, Title as Albumnaam
+FROM Artist
+RIGHT JOIN Album
+ON Album.AlbumId = Artist.ArtistId
+
 
