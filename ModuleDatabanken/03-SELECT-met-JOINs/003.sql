553f28a87714cb930daaafbba3d6223a5d4e7029
DiederikDS <diederik.desoete96@gmail.com>
2020-10-08 15:13:39 +0200

    Update 003.sql
    Oplossing oefening 3

--- ModuleDatabanken/03-SELECT-met-JOINs/003.sql
+++ ModuleDatabanken/03-SELECT-met-JOINs/003.sql
@@ -1,4 +1,17 @@
 ﻿-- Maak voor alle tracks een overzicht van tracknaam, albumnaam, artiestnaam, naam mediatype, naam genre.
 -- Hou er rekening mee dat een track niet altijd een genre heeft. Een track heeft ook niet altijd een album.
 
-
+SELECT tr.Name as Tracknaam
+	, al.Title as Albumnaam
+	, ar.Name as Artiestnaam
+	, mt.Name as Mediatype
+	, g.Name as Genre
+FROM Track as tr
+LEFT JOIN Album as al
+ON tr.AlbumId = al.AlbumId
+LEFT JOIN Artist as ar
+ON al.ArtistId = ar.ArtistId
+LEFT JOIN MediaType as mt
+ON tr.MediaTypeId = mt.MediaTypeId
+LEFT JOIN Genre as g
+ON tr.GenreId = g.GenreId
