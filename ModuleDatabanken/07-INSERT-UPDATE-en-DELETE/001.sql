5df86ce9ecaa1158597f896858067a69ba61c212
DiederikDS <diederik.desoete96@gmail.com>
2020-10-13 21:02:05 +0200

    Update 001.sql
    Oplossing 7.1

--- ModuleDatabanken/07-INSERT-UPDATE-en-DELETE/001.sql
+++ ModuleDatabanken/07-INSERT-UPDATE-en-DELETE/001.sql
@@ -1,4 +1,6 @@
 ﻿-- Gebruik letterlijke waarden in een INSERT statement om een record met je eigen naam toe te voegen aan de tabel Customers.
 -- Vul alleen de waarden in die nodig zijn.
 
+INSERT INTO Customer(CustomerId, FirstName, LastName, Email)
+VALUES ('60', 'Diederik', 'De Soete', 'diederik.desoete96@gmail.com');
 
