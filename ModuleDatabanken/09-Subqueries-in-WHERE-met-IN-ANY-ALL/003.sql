e55ba2fd747698d52c63a6bc506dc6885ba89b69
DiederikDS <diederik.desoete96@gmail.com>
2020-11-24 21:01:46 +0100

    Update 003.sql

--- ModuleDatabanken/09-Subqueries-in-WHERE-met-IN-ANY-ALL/003.sql
+++ ModuleDatabanken/09-Subqueries-in-WHERE-met-IN-ANY-ALL/003.sql
@@ -1,6 +1,19 @@
 ﻿-- Geef de voornaam en familienaam van alle klanten (Customer) waarvoor er een medewerker (Employee) bestaat met dezelfde voornaam.
 -- Los op met een niet-gelinkte subquery, gedeclareerd in een parameter (dus met DECLARE en SET).
 
+DECLARE @firstname TABLE (Firstname VARCHAR(100))
+
+INSERT INTO @firstname
+SELECT firstname
+FROM Employee
+
+SELECT firstname, lastname
+FROM Customer
+WHERE firstname IN (
+
+		SELECT *
+		FROM @firstname
+		)
 
 
 
