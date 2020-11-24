4cc2ddf70910ff6899d8df78f102849f8be644ce
DiederikDS <diederik.desoete96@gmail.com>
2020-11-24 20:32:13 +0100

    Update 001.sql

--- ModuleDatabanken/09-Subqueries-in-WHERE-met-IN-ANY-ALL/001.sql
+++ ModuleDatabanken/09-Subqueries-in-WHERE-met-IN-ANY-ALL/001.sql
@@ -1,4 +1,9 @@
 ﻿-- Hoeveel artiesten zijn er die geen album hebben? Of minder specifiek verwoord: zijn er artiesten opgeslagen in de database waarvoor er geen album is?
 -- Los op met een ANY subquery. 
 
-
+SELECT COUNT(Name)
+FROM Artist
+WHERE NOT ArtistId = ANY (
+	SELECT ArtistId
+	FROM Album
+	)
