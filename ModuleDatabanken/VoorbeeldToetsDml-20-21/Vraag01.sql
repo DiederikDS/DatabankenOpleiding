72fc9a596127d8dee8af18d1577872d2b6f22636
DiederikDS <diederik.desoete96@gmail.com>
2020-11-26 18:49:49 +0100

    Update Vraag01.sql

--- ModuleDatabanken/VoorbeeldToetsDml-20-21/Vraag01.sql
+++ ModuleDatabanken/VoorbeeldToetsDml-20-21/Vraag01.sql
@@ -7,3 +7,6 @@ Schrijf een query die je eigen voornaam in de kolom
 familienaam in de kolom [Familienaam] zet.
 
 */
+
+INSERT INTO [dbo].Table (Voornaam, Familienaam)
+VALUES ("Diederik", "De Soete");
